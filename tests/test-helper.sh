8d3878e0c707cbedba65ba8706d430574577c840
Carlos Alexandro Becker <caarlos0@gmail.com>
2014-12-30 15:19:01 -0200

    dont print wget

--- tests/test-helper.sh
+++ tests/test-helper.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 [[ ! -f "assert.sh" ]] && \
-  wget https://raw.githubusercontent.com/lehmannro/assert.sh/master/assert.sh
+  wget https://raw.githubusercontent.com/lehmannro/assert.sh/master/assert.sh \
+  &> /dev/null
 source ./assert.sh
 source ./open-pr.plugin.zsh
