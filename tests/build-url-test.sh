40f180cb862b69fc42731731201256907e32b53e
Carlos Alexandro Becker <caarlos0@gmail.com>
2016-01-27 12:17:02 -0200

    shellcheck fixes

--- tests/build-url-test.sh
+++ tests/build-url-test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=1091
 source "./tests/test-helper.sh"
 _mktemp() {
   mktemp -dt fake-repo.XXXXXXXXXX
@@ -6,7 +7,7 @@ _mktemp() {
 
 init_repo() {
   local dir="$1"
-  cd "$dir"
+  cd "$dir" || exit 1
   git init &> /dev/null
   git remote add origin git@github.com:invalid-user/invalid-repo.git
   git checkout -b feat &> /dev/null
@@ -14,14 +15,14 @@ init_repo() {
 
 add_upstream() {
   local dir="$1"
-  cd "$dir"
+  cd "$dir" || exit 1
   git remote add upstream git@github.com:upstream-invalid-user/invalid-repo.git
 }
 
 # only origin, PR to master
 dir=$(_mktemp)
 init_repo "$dir"
-cd "$dir"
+cd "$dir" || exit 1
 assert "_build_url" \
   "https://github.com/invalid-user/invalid-repo/pull/new/master...feat"
 rm -rf "$dir"
@@ -29,7 +30,7 @@ rm -rf "$dir"
 # only origin, PR to blah
 dir=$(_mktemp)
 init_repo "$dir"
-cd "$dir"
+cd "$dir" || exit 1
 assert "_build_url blah" \
   "https://github.com/invalid-user/invalid-repo/pull/new/blah...feat"
 rm -rf "$dir"
@@ -38,7 +39,7 @@ rm -rf "$dir"
 dir=$(_mktemp)
 init_repo "$dir"
 add_upstream "$dir"
-cd "$dir"
+cd "$dir" || exit 1
 assert "_build_url" \
   "https://github.com/invalid-user/invalid-repo/pull/new/upstream-invalid-user:master...invalid-user:feat"
 rm -rf "$dir"
@@ -47,7 +48,7 @@ rm -rf "$dir"
 dir=$(_mktemp)
 init_repo "$dir"
 add_upstream "$dir"
-cd "$dir"
+cd "$dir" || exit 1
 assert "_build_url blah" \
   "https://github.com/invalid-user/invalid-repo/pull/new/upstream-invalid-user:blah...invalid-user:feat"
 rm -rf "$dir"
--- tests/test-helper.sh
+++ tests/test-helper.sh
@@ -2,5 +2,7 @@
 [[ ! -f "assert.sh" ]] && \
   wget https://raw.githubusercontent.com/lehmannro/assert.sh/master/assert.sh \
   &> /dev/null
+# shellcheck disable=1091
 source ./assert.sh
+# shellcheck disable=1091
 source ./open-pr.plugin.zsh
